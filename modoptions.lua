96c463338953043eb6d34b26a591b2b77ff56926
robertthepie <stanlytheplatypus@gmail.com>
2024-04-17 12:17:27 +0100

    Add Force Load Options for Legion, Scav, & Raptors
    So that their units can be loaded in without enabling the respective ai or adding legion to the start options.

--- modoptions.lua
+++ modoptions.lua
@@ -1307,13 +1307,37 @@ local options = {
         def     = "",
     },
 	{
+		key		= "forceraptors",
+		name	= "Force Load Raptors",
+		desc	= "Load All Raptor Unit Defs even if raptor ai isn't present",
+		section = "dev",
+		type	= "bool",
+		def		= false,
+	},
+	{
+		key		= "forcescav",
+		name	= "Force Load Scavanger",
+		desc	= "Load scav units even if scav ai isn't present",
+		section = "dev",
+		type	= "bool",
+		def		= false,
+	},
+	{
+		key		= "forcelegion",
+		name	= "Force Load Legion",
+		desc	= "Load legion units even if legion isn't enabled, as such not adding it to start options but still loading the units",
+		section = "dev",
+		type	= "bool",
+		def		= false,
+	},
+	{
 		key		= "playableraptors",
 		name	= "Playable Raptors (Prototype)",
 		desc	= "Playable Raptors (Prototype), with a unique sping at an Economy and Progression.\nFaction is based around building many hives that can upgrade, rather than boosting and a singular one.",
 		type	= "bool",
 		section	= "dev",
 		def		= false,
-	}
+	},
 }
     ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
